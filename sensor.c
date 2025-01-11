47ef4c834365c21225954f06368ead9bda985bdc
Luan Galindo <132147646+luangalindo1@users.noreply.github.com>
2025-01-11 18:43:35 -0300

    Add files via upload

--- sensor.c
+++ sensor.c
@@ -37,3 +37,38 @@ float sensor_read_car_speed(int pin_a, int pin_b, float wheel_diameter) {
 
     return 0; // Se não houver transição, a velocidade é zero
 }
+
+/**
+ * @brief Lê a rotação por minuto (RPM) do motor a partir dos sinais do sensor Hall.
+ *
+ * @param pin_a Pino GPIO conectado ao canal A do sensor Hall do motor.
+ * @param pin_b Pino GPIO conectado ao canal B do sensor Hall do motor.
+ * @return Rotação por minuto (RPM) do motor.
+ */
+int sensor_read_motor_rpm(int pin_a, int pin_b) {
+    static int last_state_a = LOW; // LOW e HIGH definidos em <wiringPi.h>
+    static struct timespec last_time = {0}; // Inicializado com zeros
+    struct timespec current_time;
+
+    if (wiringPiSetupGpio() == -1) {
+        return -1; // Erro ao inicializar a biblioteca WiringPi
+    }
+
+    int current_state_a = digitalRead(pin_a);
+
+    if (current_state_a != last_state_a && current_state_a == HIGH) {
+        clock_gettime(CLOCK_MONOTONIC, &current_time);
+
+        float elapsed_time_s = (current_time.tv_sec - last_time.tv_sec) +
+                               (current_time.tv_nsec - last_time.tv_nsec) / 1e9;
+
+        last_time = current_time;
+        last_state_a = current_state_a;
+
+        // Calculo do RPM: rpm = (1 / tempo_por_revolucao) * 60
+        int rpm = (int)((1 / elapsed_time_s) * 60);
+        return rpm;
+    }
+
+    return 0; // Se não houver transição, o RPM é zero
+}
