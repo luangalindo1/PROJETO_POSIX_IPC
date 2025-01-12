5fb4017babbdbd315c374ab8665a8aadf1bee76c
Luan Galindo <132147646+luangalindo1@users.noreply.github.com>
2025-01-12 17:38:03 -0300

    Delete interrupt.h

--- interrupt.h
+++
@@ -1,8 +0,0 @@
-#ifndef INTERRUPT_H
-#define INTERRUPT_H
-
-// Configuração de Interrupções
-int interrupt_setup(int pin, int edge, void (*callback)(void));
-int interrupt_disable(int pin);
-
-#endif // INTERRUPT_H
