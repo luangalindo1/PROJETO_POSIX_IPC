472c7f1f3e8f77eaccae1b5dc8bb61ae7b1ef05b
Luan Galindo <132147646+luangalindo1@users.noreply.github.com>
2025-01-14 10:36:53 -0300

    Delete cruise_control.h

--- cruise_control.h
+++
@@ -1,9 +0,0 @@
-#ifndef CRUISE_CONTROL_H
-#define CRUISE_CONTROL_H
-
-void cruise_control_enable(float speed);
-void cruise_control_disable();
-void cruise_control_adjust_speed(int delta);
-void cruise_control_loop();
-
-#endif // CRUISE_CONTROL_H
