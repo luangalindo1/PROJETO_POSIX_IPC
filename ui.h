241c1d5494a89c4db0aa776f369739654e9afa41
Luan Galindo <132147646+luangalindo1@users.noreply.github.com>
2025-01-14 10:37:25 -0300

    Delete ui.h

--- ui.h
+++
@@ -1,8 +0,0 @@
-#ifndef UI_H
-#define UI_H
-
-void ui_display(float speed, int rpm, float distance, int cruise_enabled, int set_speed, int headlights);
-char ui_get_user_command();
-void ui_process_command(char command, int *cruise_enabled, int *set_speed, int *headlights);
-
-#endif
