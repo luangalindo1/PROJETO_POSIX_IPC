c3760fa67dc53c2c743228da9caa73842302b8c9
Luan Galindo <132147646+luangalindo1@users.noreply.github.com>
2025-01-16 09:08:36 -0300

    Add files via upload

--- controller.c
+++ controller.c
@@ -305,8 +305,8 @@ int motor_rpm() {
     
     // Cálculo do RPM
     //rpm = (motorPulsos / MOTOR_PULSOS_POR_REVOLUCAO) / deltaTempo * 60.0;
-    rpm = Pulso_Atual / deltaTempo;
-    
+    //rpm = Pulso_Atual / deltaTempo;
+    rpm = Pulso_Atual / (tempoAtual.tv_sec + (tempoAtual.tv_nsec / 1e9))*1e5;
     // Reset dos pulsos e atualização do tempo
     motorPulsos = 0;
     
